a4d1664c189cba6533a3350016009cce99a4797a
serkankonus <serkan.konus@bulutmd.com>
2023-01-17 02:53:32 +0300

    union, intersect, except

--- sql_join.sql
+++ sql_join.sql
@@ -23,3 +23,34 @@ RIGHT JOIN customer ON customer.customer_id = payment.customer_id;
 -- Example 3
 SELECT rental_id, first_name, last_name FROM rental
 FULL JOIN customer ON customer.customer_id=rental.customer_id;
+
+-- UNION | INTERSECT | EXCEPT
+-- Example 1
+(SELECT first_name FROM actor ORDER BY first_name)
+UNION
+(SELECT first_name FROM customer ORDER BY first_name);
+
+-- Example 2
+(SELECT first_name FROM actor ORDER BY first_name)
+INTERSECT
+(SELECT first_name FROM customer ORDER BY first_name);
+
+-- Example 3
+(SELECT first_name FROM actor ORDER BY first_name)
+EXCEPT
+(SELECT first_name FROM customer ORDER BY first_name);
+
+-- Example 4
+(SELECT first_name FROM actor ORDER BY first_name)
+UNION ALL
+(SELECT first_name FROM customer ORDER BY first_name);
+
+-- Example 5
+(SELECT first_name FROM actor ORDER BY first_name)
+INTERSECT ALL
+(SELECT first_name FROM customer ORDER BY first_name);
+
+-- Example 6
+(SELECT first_name FROM actor ORDER BY first_name)
+EXCEPT ALL
+(SELECT first_name FROM customer ORDER BY first_name);
